71fcc949199760c439b64d11448a3595fa91b500
Pro. IT-Developer <44401041+ProITDeveloper@users.noreply.github.com>
2019-10-18 00:12:31 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -18,13 +18,18 @@
  * @package WordPress
  */
 
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'PakTraveler' );
+
 /** MySQL database username */
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', '' );
 
-
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
@@ -41,7 +46,45 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
+define( 'AUTH_KEY',         's9XTCv 0ZZ[u9xN&F+kp11*Pz+7g4i2!An:RySzSNtbP>ISr9ghEfPbR^A)]]!L*' );
+define( 'SECURE_AUTH_KEY',  'OE1>kHYzEuGt9;<<-rX9dSctxS0d]cv,mf?eI^La3!:u_ty[!GK12P*66A@bP+xi' );
+define( 'LOGGED_IN_KEY',    'eH_,b%}D7_PScl MkX9$_M X3GN4bW[XqsHaVhG;xcPw=k7S7C=~P=LR <Gb;5U_' );
+define( 'NONCE_KEY',        'w&::Z,ojZdip|*E}SwGvejz%lJv7uCC_P:{1&8p6E2XA{wO6LbPCI-5wbBHwVE+l' );
+define( 'AUTH_SALT',        '.y)yLe%7re{]}jIp=Df0JCBJ}2Z(If73f>ZxhjZtvJhf=Au@am:xYj^H)6Hgk5YM' );
+define( 'SECURE_AUTH_SALT', 'm0;/386k[|Mx&nTWXN`PU#>@i]Ec%|WzkV<Qc[&VUB,=O7f+y2/(Vze+v%v<ngL=' );
+define( 'LOGGED_IN_SALT',   'oJRWENI%lX$ >O$kGe4a[%f]Vv1g-fDU[QzF,cl&CBH_,`LInQc~|9)U*G.xe(q8' );
+define( 'NONCE_SALT',       'Zxy>qh[ebw<dIL4<8MNzQ#iW(yH@OB5T7]#`n<hSj}5w&j2iVxt(&Zz&QR!^Q(bo' );
 
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
