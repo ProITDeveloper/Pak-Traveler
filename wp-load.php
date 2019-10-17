2731ff25032a23ba7970811c455082bcc6fb036d
ProITDeveloper <proitdeveloper@gmail.com>
2019-10-18 00:20:05 +0500

    Addition of third set of data files

---
+++ wp-load.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Bootstrap file for setting the ABSPATH constant
+ * and loading the wp-config.php file. The wp-config.php
+ * file will then load the wp-settings.php file, which
+ * will then set up the WordPress environment.
+ *
+ * If the wp-config.php file is not found then an error
+ * will be displayed asking the visitor to set up the
+ * wp-config.php file.
+ *
+ * Will also search for wp-config.php in WordPress' parent
+ * directory to allow the WordPress directory to remain
+ * untouched.
+ *
+ * @package WordPress
+ */
+
+/** Define ABSPATH as this file's directory */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
+
+
+	// A config file doesn't exist
+
+	define( 'WPINC', 'wp-includes' );
+	require_once( ABSPATH . WPINC . '/load.php' );
+
+	// Standardize $_SERVER variables across setups.
+	wp_fix_server_vars();
+
+	require_once( ABSPATH . WPINC . '/functions.php' );
+
+	$path = wp_guess_url() . '/wp-admin/setup-config.php';
+
+	/*
+	 * We're going to redirect to setup-config.php. While this shouldn't result
+	 * in an infinite loop, that's a silly thing to assume, don't you think? If
+	 * we're traveling in circles, our last-ditch effort is "Need more help?"
+	 */
+	if ( false === strpos( $_SERVER['REQUEST_URI'], 'setup-config' ) ) {
+		header( 'Location: ' . $path );
+		exit;
+	}
+
+	wp_die( $die, __( 'WordPress &rsaquo; Error' ) );
+}
